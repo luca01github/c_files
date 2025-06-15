feb8e5c2f31ff443732c81551bd160aeeb6e2d7e
luca01github <luca.porcheddu01@gmail.com>
2025-06-15 17:22:15 +0200

    Update inputSicuro.c with comments

--- inputSicuro.c
+++ inputSicuro.c
@@ -4,7 +4,8 @@
 #include <errno.h>
 #include <limits.h>
 
-#define INPUT_SIZE 32
+//è buona pratica avere almeno il doppio della dimensione necessaria
+#define INPUT_SIZE 32    
 
 int leggi_intero(int min, int max){
 	char input[INPUT_SIZE];
@@ -19,21 +20,22 @@ int leggi_intero(int min, int max){
 			exit(EXIT_FAILURE);
 		}
 
-		if(strchr(input, '\n') == NULL){
+		if(strchr(input, '\n') == NULL){  //Se nell'input non viene trovato '\n' (perché input troppo grande)
 			int ch;
-			while((ch = getchar()) != '\n' && ch != EOF);
+			while((ch = getchar()) != '\n' && ch != EOF);  //Pulisce il buffer
 		}
 
-		input[strcspn(input, "\n")] = '\0';
+		input[strcspn(input, "\n")] = '\0';   //Mette il carattere di terminazione delle stringhe '\0' al posto del newLine
 
-		errno = 0;
-		numero = strtol(input, &rimanente, 10);
+		errno = 0;      //Imposta a zero la variabile errno, che viene modificata se una conversione non va a buon fine
+		//mette in "rimanente" i caratteri che non sono numeri, ad esempio se si inserisce 123ab
+		numero = strtol(input, &rimanente, 10);  //10 è la base
 
-		if(errno != 0){
+		if(errno != 0){  //se errno viene modificato la conversione strtol non è andata a buon fine
 			perror("Errore di conversione");
 			continue;
 		}
-		if(*rimanente != '\0'){
+		if(*rimanente != '\0'){   //se in rimanente ci sono delle lettere significa che sono state inserite nell'input
 			puts("Input non valido: sono ammessi solo numeri interi");
 			continue;
 		}
@@ -41,11 +43,11 @@ int leggi_intero(int min, int max){
 			printf("Errore, il numero deve essere compreso tra %d e %d\n", min, max);
 			continue;
 		}
-		if(numero<INT_MIN || numero>INT_MAX){
+		if(numero<INT_MIN || numero>INT_MAX){   //INT_MIN e INT_MAX fanno parte di <limits.h>
 			puts("Errore: il numero inserito è fuori dai limiti del tipo int");
 			continue;
 		}
-		return (int)numero;
+		return (int)numero;  //strtol restituisce un long int, per cui si fa la conversione a int
 	}
 }
 

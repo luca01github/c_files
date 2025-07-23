bd33b379b8482cce9f7c3e33caa52d332cc079d8
luca01github <luca.porcheddu01@gmail.com>
2025-07-24 00:29:25 +0200

    Add files via upload

---
+++ vigenere.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+int isValid(char* key);
+void encrypt(char* cleartext, char* encrypted_text, char* key);
+void decrypt(char* encrypted_text, char* decypted_text, char* key);
+
+int main(){
+    char key[] = "XMWUJBVYHXZ";
+    char* text = "Welcome to encryption";
+    char encrypted_str[100] = "";
+    char decrypted_str[100] = "";
+    
+    if(isValid(key)){
+        printf("Original text: %s\n", text);
+        puts("Encrypted text:");
+        encrypt(text, encrypted_str, key);
+        printf("%s\n", encrypted_str);
+        puts("Decrypted text:");
+        decrypt(encrypted_str, decrypted_str, key);
+        printf("%s\n", decrypted_str);
+    }
+}
+
+//Checks if the key contains letters only
+int isValid(char* key){
+    int i=0;
+    while(key[i] != '\0'){
+        if(!isalpha(key[i])){
+            return 0;
+        }
+        i++;
+    }
+    return 1;
+}
+
+char getSubstitution(char keyChar, char c, char cr_or_decr){
+    static const char table[26][26] = {
+        
+        {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' },
+        {'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A' },
+        {'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B' },
+        {'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C' },
+        {'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D' },
+        {'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E' },
+        {'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F' },
+        {'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G' },
+        {'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H' },
+        {'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I' },
+        {'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' },
+        {'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K' },
+        {'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L' },
+        {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M' },
+        {'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N' },
+        {'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O' },
+        {'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P' },
+        {'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q' },
+        {'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R' },
+        {'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S' },
+        {'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T' },
+        {'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U' },
+        {'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V' },
+        {'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W' },
+        {'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X' },
+        {'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y' }
+    };
+    
+    char letter_case = isupper(c) ? 'u' : 'l';
+    c = toupper(c);
+
+    //encrypting
+    if(cr_or_decr == 1){
+        int row = keyChar - 'A';
+        int col = c - 'A';
+        return (letter_case == 'u') ? table[row][col] : tolower(table[row][col]);
+    }
+    //decrypting
+    else{
+        int row = keyChar - 'A';
+        for(int col=0; col<26; col++){
+            if(table[row][col] == c){
+                return (letter_case == 'u') ? 'A' + col : tolower('A' + col);
+            }
+        }
+    }
+    return c;  //fallback
+}
+
+/*
+    @param the clear text to encrypt
+    @param the array of char in which I write the encrypted string
+    @param the key
+*/
+void encrypt(char* cleartext, char* encrypted_text, char* key){
+    int key_len = strlen(key);
+    int j=0; 
+    for(int i=0; i<strlen(cleartext); i++){
+        if(isalpha(cleartext[i])){
+            //switch the letter with the corrispondent char of the table 
+            encrypted_text[i] = getSubstitution(key[j % key_len], cleartext[i], 1);
+            j++;  //Advance only if there is a letter
+        }
+        else{
+            encrypted_text[i] = cleartext[i];  //keep the symbol
+        }        
+    }
+    encrypted_text[strlen(cleartext)] = '\0';
+}
+
+/*
+    @param encrypted text to decrypt
+    @param a string in which I write the decrypted string
+    @param the key
+*/
+void decrypt(char* encrypted_text, char* decrypted_text, char* key){
+    int key_len = strlen(key);
+    int j=0; 
+
+    for(int i=0; i<strlen(encrypted_text); i++){
+        if(isalpha(encrypted_text[i])){
+            decrypted_text[i] = getSubstitution(key[j % key_len], encrypted_text[i], 0);
+            j++;
+        }
+        else{
+            decrypted_text[i] = encrypted_text[i];
+        }
+    }
+    decrypted_text[strlen(decrypted_text)] = '\0';
+}
